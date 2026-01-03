a257ee7704c497f83686157199ffc8da3bd09e6f
Amit Chaudhary <amitness@users.noreply.huggingface.co>
2026-01-03 10:35:50 +0000

    Arxiv tar file download endpoint API update (#1)
    - Arxiv tar file download endpoint API update (28155c8791a79a3e24545ebf0b16c3b4f03970de)
    
    
    Co-authored-by: bacmive_tam <bacmive@users.noreply.huggingface.co>

--- app.py
+++ app.py
@@ -9,14 +9,13 @@ import requests
 
 
 def convert_tar_to_zip(arxiv_url):
-    latex_source_url = (arxiv_url.replace('/abs/', '/e-print/')
-                        .replace('arxiv.org', 'export.arxiv.org'))
+    latex_source_url = (arxiv_url.replace('/abs/', 'src'))
     
     # Fetch the latex source as .tar.gz file
     resp = requests.get(latex_source_url)
     print(resp.status_code)
     tar_file = resp.content
-    with tarfile.open(fileobj=io.BytesIO(tar_file)) as tar:
+    with tarfile.open(fileobj=io.BytesIO(tar_file), mode='r:gz') as tar:
         with tempfile.TemporaryDirectory() as temp_dir:
             # Extract the tar file to a temporary directory
             tar.extractall(temp_dir)
